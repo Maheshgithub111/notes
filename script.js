9e5bf84ae95c3a4b9ffa126b4cba06bee813e61d
Mahesh Singh <maheshhsingh69@gmail.com>
2024-09-03 21:46:52 +0530

    notes are saved in the local storage

--- script.js
+++ script.js
@@ -1,60 +1,81 @@
-const createBtn = document.querySelector(".add-btn");
+const createBtn = document.getElementById("add-btn");
 const containerMain = document.querySelector(".container-main");
 const noteContainer = document.getElementById("jsNoteAll");
 const form = document.getElementById("input-form");
 const inputTitle = document.getElementById("js-input-title");
 const titleNote = document.getElementById("title-note");
 const bodyNote = document.getElementById("js-input-note");
+const closeBtnElement = document.getElementById("close-btn");
 
-const data =[];
+const data = [];
 
-function inputNoteClicked(){
-     document.getElementById('js-input-title').classList.remove('title-input');
-     document.getElementById('close-btn').classList.remove('btn-display');
-     document.getElementById('add-btn').classList.remove('btn-display');
+const createNote = (note) => {
+     noteContainer.innerHTML += `
+        <div class="note-container">
+             <div class="title-note">${note.title}</div>
+                  <div class="note-text">${note.note}</div>
+                      <img class="rewrite-icon" width="20" height="20" src="https://img.icons8.com/pastel-glyph/128/FFFFFF/edit--v1.png" alt="edit--v1"/>
+                  <img class="trash-bin" width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/filled-trash.png" alt="filled-trash"/>
+             
+        </div>`;
+
+     inputTitle.value = "";
+     bodyNote.value = "";
 };
+ 
 
-function buttonfun(){
-     document.getElementById('js-input-title').classList.add('title-input');
-     document.getElementById('close-btn').classList.add('btn-display');
-     document.getElementById('add-btn').classList.add('btn-display');
-     };
-console.log('working');
+function inputNoteClicked() {
+  document.getElementById("js-input-title").classList.remove("title-input");
+  document.getElementById("close-btn").classList.remove("btn-display");
+  document.getElementById("add-btn").classList.remove("btn-display");
+}
 
-//an eventlistener for add-btn
-form.addEventListener("submit", (e)=> {
-     e.preventDefault();
-     formValidation();
+function buttonfun() {
+  document.getElementById("js-input-title").classList.add("title-input");
+  document.getElementById("close-btn").classList.add("btn-display");
+  document.getElementById("add-btn").classList.add("btn-display");
+}
+
+// an eventlistener for close-btn
+closeBtnElement.addEventListener("click", (e) => {
+  buttonfun();
+  e.preventDefault();
+  inputTitle.value = "";
+  bodyNote.value = "";
 });
 
+//an eventlistener for add-btn
+createBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formValidation();
+  buttonfun();
+});
+
+function renderData() {
+  const data = JSON.parse(localStorage.getItem("data")) || [];
+  data.forEach(createNote);
+}
+
 // just a arrow function
-const formValidation = ()=>{
-     if (inputTitle.value === "") {
-          console.log("empty");
-     }
-     else{
-          acceptData();
-     }
+const formValidation = () => {
+  if (inputTitle.value === "") {
+    console.log("empty");
+  } else {
+    acceptData();
+    localStorage.setItem("data", JSON.stringify(data));
+  }
 };
 
 const acceptData = () => {
-     data["title"] = inputTitle.value;
-     data["note"] = bodyNote.value;
-     console.log(data);
-     createNote();
+  const note = {
+    title: inputTitle.value,
+    note: bodyNote.value,
+  };
+  data.push(note);
+  console.log(data);
+  createNote(note);
+  renderData();
 };
 
-const createNote = () =>{
-noteContainer.innerHTML +=`
-     <div class="note-container">
-          <div class="title-note">${data.title}</div>
-          <div class="note-text">${data.note}</div>
-     </div>
-`;    noteContainer.style.height = `${noteText.offsetHeight}px`;
-     inputTitle.value = '';
-     bodyNote.value = '';
-}
-
-const noteText = document.getElementById("note-text");
-
+renderData();
 
