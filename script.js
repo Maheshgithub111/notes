256ffc4af6c53b5a92824a8e1a3893cd9846ca2d
Mahesh Singh <maheshhsingh69@gmail.com>
2024-09-11 17:44:52 +0530

    fixed the delete fun and depoled on vercel

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const myUUID = uuidv4();
 const rewriteBtn = document.querySelector(".rewrite-icon");
 const focusedContainerMain = document.querySelector(".focusedcontainermain");
 
-const data = [];
+let data = [];
 
 function inputNoteClicked() {
   document.getElementById("js-input-title").classList.remove("title-input");
@@ -24,7 +24,6 @@ function buttonfun() {
   document.getElementById("add-btn").classList.add("btn-display");
 }
 
-// an eventlistener for close-btn
 closeBtnElement.addEventListener("click", (e) => {
   buttonfun();
   e.preventDefault();
@@ -32,19 +31,14 @@ closeBtnElement.addEventListener("click", (e) => {
   bodyNote.value = "";
 });
 
-//addnote, savenote,
-
 const createNoteTemplate = (note) => {
-  noteContainer.innerHTML += `
-        <div class="note-container">
-             <div class="title-note">${note.title}</div>
-                  <div class="note-text">${note.note}</div>
-                      <img class="rewrite-icon" onclick = "editClicked('${note.id}')" width="20" height="20" src="https://img.icons8.com/pastel-glyph/128/FFFFFF/edit--v1.png" alt="edit--v1"/>
-                  <img class="trash-bin" onclick= "deleteNote('${note.id}')" width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/filled-trash.png" alt="filled-trash"/>
-        </div>`;
-
-  inputTitle.value = "";
-  bodyNote.value = "";
+  return `
+    <div class="note-container">
+         <div class="title-note">${note.title}</div>
+              <div class="note-text">${note.note}</div>
+                  <img class="rewrite-icon" onclick="editClicked('${note.id}')" width="20" height="20" src="https://img.icons8.com/pastel-glyph/128/FFFFFF/edit--v1.png" alt="edit--v1"/>
+              <img class="trash-bin" onclick="deleteNote('${note.id}')" width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/filled-trash.png" alt="filled-trash"/>
+    </div>`;
 };
 
 const acceptData = () => {
@@ -54,31 +48,28 @@ const acceptData = () => {
     id: uuidv4(),
   };
   data.push(note);
-  console.log(data);
-  createNoteTemplate(note);
+  localStorage.setItem("data", JSON.stringify(data));
+  renderNote();
+  console.log(note);
+  inputTitle.value = "";
+  bodyNote.value = "";
 };
 
-//an eventlistener for add-btn
 createBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  formValidation(); //to validate the input(note) and accept data;
-  buttonfun(); //to close the top section
+  formValidation();
+  buttonfun();
 });
 
-// just a arrow function
 const formValidation = () => {
-  if (inputTitle.value === "") {
-    console.log("empty");
-  } else {
+  if (inputTitle.value !== "") {
     acceptData();
-    localStorage.setItem("data", JSON.stringify(data));
+  } else {
+    console.log("empty");
   }
 };
 
-//fix deletefun
-
 function deleteNote(id) {
-  let data = JSON.parse(localStorage.getItem("data"));
   data = data.filter((note) => note.id !== id);
   localStorage.setItem("data", JSON.stringify(data));
   renderNote();
@@ -86,67 +77,62 @@ function deleteNote(id) {
 }
 
 function editClicked(id) {
-     // Get the data from localStorage
-     const data = JSON.parse(localStorage.getItem("data")) || [];
-     const noteToEdit = data.find((note) => note.id === id);
+  const noteToEdit = data.find((note) => note.id === id);
    
-     if (!noteToEdit) {
-       console.error("Note not found");
-       return;
-     }
+  if (!noteToEdit) {
+    console.error("Note not found");
+    return;
+  }
    
-     // Get the focused container
-     const focusedContainerMain = document.getElementById("focusedcontainermain");
+  const focusedContainerMain = document.getElementById("focusedcontainermain");
      
-     // Create and display the focused container
-     focusedContainerMain.innerHTML = `
-       <div class="focusedContainer">
-         <div class="title" contenteditable="true">${noteToEdit.title}</div>
-         <div class="note" contenteditable="true">${noteToEdit.note}</div>
-         <div class="buttonFocusedContainer">    
-           <button class="savebtnOnFocused" onclick="saveEditedNote('${id}')">Save</button>
-           <button class="closebtnOnFocused" onclick="closeFocusedContainer()">Close</button>
-         </div>
-       </div>
-     `;
+  focusedContainerMain.innerHTML = `
+    <div class="focusedContainer">
+      <div class="title" contenteditable="true">${noteToEdit.title}</div>
+      <div class="note" contenteditable="true">${noteToEdit.note}</div>
+      <div class="buttonFocusedContainer">    
+        <button class="savebtnOnFocused" onclick="saveEditedNote('${id}')">Save</button>
+        <button class="closebtnOnFocused" onclick="closeFocusedContainer()">Close</button>
+      </div>
+    </div>
+  `;
    
-     // Show the focused container
-     focusedContainerMain.style.display = "flex";
-     focusedContainerMain.style.zIndex = "10";
-   }
+  focusedContainerMain.style.display = "flex";
+  focusedContainerMain.style.zIndex = "10";
+}
    
-   function closeFocusedContainer() {
-     const focusedContainerMain = document.getElementById("focusedcontainermain");
-     focusedContainerMain.style.display = "none";
-     focusedContainerMain.style.zIndex = "-1";
-   }
+function closeFocusedContainer() {
+  const focusedContainerMain = document.getElementById("focusedcontainermain");
+  focusedContainerMain.style.display = "none";
+  focusedContainerMain.style.zIndex = "-1";
+}
    
-   function saveEditedNote(id) {
-     const focusedContainer = document.querySelector(".focusedContainer");
-     const editedTitle = focusedContainer.querySelector(".title").textContent;
-     const editedNote = focusedContainer.querySelector(".note").textContent;
+function saveEditedNote(id) {
+  const focusedContainer = document.querySelector(".focusedContainer");
+  const editedTitle = focusedContainer.querySelector(".title").textContent;
+  const editedNote = focusedContainer.querySelector(".note").textContent;
    
-     let data = JSON.parse(localStorage.getItem("data")) || [];
-     const noteIndex = data.findIndex(note => note.id === id);
+  const noteIndex = data.findIndex(note => note.id === id);
    
-     if (noteIndex !== -1) {
-       data[noteIndex].title = editedTitle;
-       data[noteIndex].note = editedNote;
-       localStorage.setItem("data", JSON.stringify(data));
-       renderNote();
-       closeFocusedContainer();
-     } else {
-       console.error("Note not found");
-     }
-   }
-
-function renderNote() {
-  const data = JSON.parse(localStorage.getItem("data")) || [];
-  noteContainer.innerHTML = "";
-  data.forEach(createNoteTemplate);
+  if (noteIndex !== -1) {
+    data[noteIndex].title = editedTitle;
+    data[noteIndex].note = editedNote;
+    localStorage.setItem("data", JSON.stringify(data));
+    renderNote();
+    closeFocusedContainer();
+  } else {
+    console.error("Note not found");
+  }
 }
 
-// id for each
+function renderNote() {
+  noteContainer.innerHTML = "";
+  data.forEach(note => {
+    noteContainer.innerHTML += createNoteTemplate(note);
+  });
+  
+}
+
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
@@ -155,4 +141,6 @@ function uuidv4() {
   });
 }
 
+// Load data from localStorage on page load
+data = JSON.parse(localStorage.getItem("data")) || [];
 renderNote();
